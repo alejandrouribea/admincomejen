2d3a889d51291a921a86b0cda90312d68aec2a0c
JohnE <edison3814@gmail.com>
2022-08-23 22:40:44 -0500

    first commit

---
+++ src/main/java/com/teamhub/admincomejen/AdmincomejenApplication.java
@@ -0,0 +1,13 @@
+package com.teamhub.admincomejen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmincomejenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmincomejenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamhub/admincomejen/AdmincomejenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamhub.admincomejen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmincomejenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
